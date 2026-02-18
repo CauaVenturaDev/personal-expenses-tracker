91ed586c3fdd2004f0f2565897a3c87554412953
Cauã Ventura <131213154+CauaVenturaDev@users.noreply.github.com>
2026-02-18 11:48:27 -0300

    Adiciona EF Core e modelos ao projeto Data
    Inclui classes Expense, Income e PersonalExpensesTrackerContext para mapear tabelas do PostgreSQL via EF Core. Atualiza o .csproj com pacotes do EF Core e Npgsql. Adiciona referências de projeto no Api e corrige caminho do Data na solução. Remove arquivos de classe vazios.

---
+++ personal-expenses-tracker.Api/src/personalExpensesTracker.Data/Scaffold/Income.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace personalExpensesTracker.Data.Scaffold;
+
+public partial class Income
+{
+    public int Id { get; set; }
+
+    public string Description { get; set; } = null!;
+
+    public decimal Amount { get; set; }
+
+    public string Category { get; set; } = null!;
+
+    public DateOnly Date { get; set; }
+}
---
+++ personal-expenses-tracker.Api/src/personalExpensesTracker.Data/Scaffold/PersonalExpensesTrackerContext.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace personalExpensesTracker.Data.Scaffold;
+
+public partial class PersonalExpensesTrackerContext : DbContext
+{
+    public PersonalExpensesTrackerContext()
+    {
+    }
+
+    public PersonalExpensesTrackerContext(DbContextOptions<PersonalExpensesTrackerContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Expense> Expenses { get; set; }
+
+    public virtual DbSet<Income> Incomes { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseNpgsql("Server=localhost;Port=5500;Database=personal-expenses-tracker;User Id=admin;Password=admin;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Expense>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("expenses_pkey");
+
+            entity.ToTable("expenses");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Amount)
+                .HasPrecision(10, 2)
+                .HasColumnName("amount");
+            entity.Property(e => e.Category)
+                .HasMaxLength(255)
+                .HasColumnName("category");
+            entity.Property(e => e.Date).HasColumnName("date");
+            entity.Property(e => e.Description)
+                .HasMaxLength(255)
+                .HasColumnName("description");
+        });
+
+        modelBuilder.Entity<Income>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("income_pkey");
+
+            entity.ToTable("income");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Amount)
+                .HasPrecision(10, 2)
+                .HasColumnName("amount");
+            entity.Property(e => e.Category)
+                .HasMaxLength(255)
+                .HasColumnName("category");
+            entity.Property(e => e.Date).HasColumnName("date");
+            entity.Property(e => e.Description)
+                .HasMaxLength(255)
+                .HasColumnName("description");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
